25bbedbae2f385aad52583bf8436bc083b8cf8da
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-03-22 15:29:19 +0300

    StartThisProject

---
+++ kurs/Door.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kurs
+{
+    /// <summary>
+    /// Логика взаимодействия для Door.xaml
+    /// </summary>
+    public partial class Door : Window
+    {
+        public Door()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnDoor_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Registr_Click(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Show();
+            Close();
+        }
+    }
+}
